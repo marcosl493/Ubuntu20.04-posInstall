8d48948fee314bc7ffbf16515dc428caac94c5a3
marcosl493 <66807489+marcosl493@users.noreply.github.com>
2021-02-19 19:25:45 -0300

    Remoção dos pacotes flatpak, etc
    Alteração na versão do .NET Sdk e adição dos pacotes snap para substituir os flatpaks

--- Pos_Install_Ubuntu-20-04.sh
+++ Pos_Install_Ubuntu-20-04.sh
@@ -20,7 +20,6 @@ mkdir -p TempFiles
 ##Indo para a pasta criada e baixando os arquivos
 ##going to the created folder and making file downloads
 cd TempFiles
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 ##Instalando os pacotes .deb baixados anteriormente e tema
 ##Installing previously downloaded packages and theme
@@ -34,7 +33,7 @@ sudo add-apt-repository ppa:lutris-team/lutris -y
 sudo add-apt-repository ppa:kdenlive/kdenlive-stable -y
 wget -nc https://dl.winehq.org/wine-builds/winehq.key 
 sudo apt-key add winehq.key 
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
 
 ##Atualização dos repositórios
 ##Update of repositories
@@ -52,7 +51,6 @@ sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386
 sudo apt install steam-installer -y
 sudo apt install steam-devices -y
 sudo apt install steam:i368 -y
-sudo apt install snapd -y
 sudo apt install libvulkan1 -y 
 sudo apt install libvulkan1:i368 -y
 sudo apt install libgnutls30:i386 -y 
@@ -64,9 +62,9 @@ sudo apt install libsdl2-2.0-0:i386 -y
 sudo apt install libfreetype6:i386 -y
 sudo apt install libdbus-1-3:i386 -y
 sudo apt install libsqlite3-0:i386 -y
+sudo apt install mesa-vulkan-drivers vulkan-utils -y
 sudo apt install flatpak -y
-sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common git vim-gtk3 curl -y
-sudo apt install tmux -y
+sudo apt install build-essential default-jdk fontconfig software-properties-common git curl -y
 sudo apt install ubuntu-restricted-extras -y
 sudo apt install audacity -y
 sudo apt install gnome-tweak-tool -y
@@ -74,24 +72,15 @@ sudo apt install unzip -y
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-3.1
+  sudo apt-get install -y dotnet-sdk-5.0
 
 ##Instalando pacotes snap
 ##Installing snap packages
 sudo snap install photogimp
 sudo snap install spotify
-##Adicionando flathub e Instalando pacotes flatpak
-##Adding flathub and installing flatpak packages
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.codeblocks.codeblocks -y
-flatpak install flathub org.eclipse.Java -y
-flatpak install flathub fr.handbrake.ghb -y
-
-##Instalando asdf
-##Installing asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
+sudo snap install eclipse --classic
+sudo snap install handbrake-jz
+sudo snap install netbeans --classic
 
 ##Conclusão, atualização e limpeza
 ##Completion, update and cleaning
